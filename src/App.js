3edac4e57e51880fa096c1bccc4e4eeebb30f321
Vincent van Rootselaar <86999767+vincentvanrootselaar@users.noreply.github.com>
2022-07-31 17:25:25 +0200

    20220731

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { Fragment} from "react";
+import axios from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
 
-export default App;
+    this.state ={
+      pages: []
+    };
+  }
+  getAllPages = async() => {
+    let res = await axios.get(
+      `http://headlessreact.local/?rest_route=/wp/v2/pages`
+    );
+    let{data} = await res;
+    this.setState({pages: data});
+  };
+
+  componentDidMount = async () => {
+    await this.getAllPages();
+  };
+  render(){
+    const {pages} = this.state;
+
+    return(
+      <Fragment>
+        {pages.map((page, index) => {
+          return <h1>
+            {page.slug}
+          </h1>;
+        })}
+      </Fragment>
+    );
+  }
+
+
+
+}
